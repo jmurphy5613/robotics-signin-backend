7f0a8462c71fa75221158c8822191a7c0c4d994a
jmurphy5613 <jmurphy5613@gmail.com>
2022-12-27 15:32:53 -0500

    set up seed

---
+++ src/seed.ts
@@ -0,0 +1,37 @@
+import { PrismaClient } from "@prisma/client"
+import { format } from "date-fns"
+
+const prisma = new PrismaClient()
+
+async function main() {
+    await prisma.user.deleteMany({})
+    await prisma.events.deleteMany({})
+
+    const today = new Date()
+
+    const createUserAndEvents = await prisma.user.create({
+        data: {
+            firstName: "john",
+            lastName: "murphy",
+            email: "jmurphy5613@gmail.com",
+            eventsAttended: {
+                create: [
+                    {
+                        startDate: today,
+                        endDate: today,
+                        title: "Robotics Meeting",
+                        code: 11111
+                    }
+                ]
+            }
+        }
+    })
+}
+
+main()
+    .catch((err) => {
+        console.log(err)
+    })
+    .finally(() => {
+        prisma.$disconnect
+    })
